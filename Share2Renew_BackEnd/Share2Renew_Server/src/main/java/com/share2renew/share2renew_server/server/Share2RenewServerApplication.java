c58693485922dda852c39902e81adef2118ee07f
Jcc-C <82298537+Jcc-C@users.noreply.github.com>
2023-09-06 12:47:14 +1000

    Add files via upload

---
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/share2renew_server/server/Share2RenewServerApplication.java
@@ -0,0 +1,17 @@
+package com.share2renew.share2renew_server.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动器
+ */
+
+@SpringBootApplication
+public class Share2RenewServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Share2RenewServerApplication.class, args);
+    }
+
+}
---
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/share2renew_server/server/service/testService.java
@@ -0,0 +1,7 @@
+package com.share2renew.share2renew_server.server.service;
+
+/**
+ * 这个接口没用的, 仅做测试
+ */
+public interface testService {
+}
