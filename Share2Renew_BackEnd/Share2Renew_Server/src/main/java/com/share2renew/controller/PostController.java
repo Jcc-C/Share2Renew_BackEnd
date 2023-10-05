896f56589c3b63c74804a4dab2c115cd5475e51a
Kevin <kevin@163.com>
2023-10-05 15:34:03 +1100

    新增分页拦截器，初步完成显示所有帖子的功能

--- Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/controller/PostController.java
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/controller/PostController.java
@@ -1,9 +1,24 @@
 package com.share2renew.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.share2renew.pojo.GeneralBean;
+import com.share2renew.pojo.Post;
+import com.share2renew.service.IPostService;
+import com.share2renew.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -15,9 +30,36 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/post")
+@Api(tags = "PostController")
 public class PostController {
     /**
      * Kevin
-     */
+     * */
 
+    @Autowired
+    private IPostService postService;
+
+    /**
+     *
+     * @param pageNo 显示第几页
+     * @param pageSize 每页显示多少条
+     * @return data
+     */
+    @PostMapping("/show_all")
+    @ApiOperation(value = "show all the posts")
+    public GeneralBean GetAllThePost(@RequestParam(value = "pageNo") Long pageNo,
+                                     @RequestParam(value = "pageSize") Long pageSize,
+                                     @RequestParam(value = "title", required = false) String title){
+
+        LambdaQueryWrapper<Post> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StringUtils.hasLength(title), Post::getPostTitle,title); //条件查询
+
+        Page<Post> page = new Page<>(pageNo, pageSize);
+        postService.page(page,wrapper);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("total", page.getTotal());//查询的总数
+        data.put("rows",page.getRecords());//查询的数据
+        return GeneralBean.success(data);
+    }
 }
