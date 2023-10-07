38aafe4e0fbf3fbbf8f216bdec00c7567cc7fcbf
jimmyC <347495739@qq.com>
2023-10-07 22:21:41 +1100

    添加Admin和PostImage相关文件

---
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/service/IPostImageService.java
@@ -0,0 +1,16 @@
+package com.share2renew.service;
+
+import com.share2renew.pojo.PostImage;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Junxian Cai
+ * @since 2023-10-07
+ */
+public interface IPostImageService extends IService<PostImage> {
+
+}
