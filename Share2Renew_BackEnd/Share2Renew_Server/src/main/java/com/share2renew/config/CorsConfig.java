773dfdd7aa0acf524680c52a77a24ff8913cb5e6
Kevin <kevin@163.com>
2023-10-09 21:32:29 +1100

    新增跨域配置文件 CorsConfig

---
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.share2renew.config;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域处理的配置文件 kevin
+ */
+public class CorsConfig {
+    public CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:8888");//允许谁异步访问  前端 端口号
+        corsConfiguration.setAllowCredentials(true);//允许cookie跨域
+        corsConfiguration.addAllowedMethod("*");//允许什么方法跨域
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
