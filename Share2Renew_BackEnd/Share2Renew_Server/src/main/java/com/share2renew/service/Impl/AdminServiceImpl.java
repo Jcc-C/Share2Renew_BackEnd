cfae03438242cc464e8fa667c97bcd155a532a5a
Leo <mzhe0637@uni.sydney.edu.au>
2023-10-10 02:44:22 +1100

    更新Admin相关的接口，但其功能还未实现

--- Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/service/Impl/AdminServiceImpl.java
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/service/Impl/AdminServiceImpl.java
@@ -2,9 +2,13 @@ package com.share2renew.service.Impl;
 
 import com.share2renew.pojo.Admin;
 import com.share2renew.mapper.AdminMapper;
+import com.share2renew.pojo.Post;
+import com.share2renew.pojo.User;
 import com.share2renew.service.IAdminService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +20,36 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AdminServiceImpl extends ServiceImpl<AdminMapper, Admin> implements IAdminService {
+
+
+    @Override
+    public Admin adminLogin(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public void modifyPostValidity(String postId) {
+
+    }
+
+    @Override
+    public void modifyUserValidity(String userId) {
+
+    }
+
+    @Override
+    public void modifyUserCommentRight(String userId) {
+
+    }
 
 }
