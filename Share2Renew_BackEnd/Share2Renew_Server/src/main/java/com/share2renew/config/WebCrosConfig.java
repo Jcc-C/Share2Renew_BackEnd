2962c0c5c1f3e272403e2dcfa00fdc5b4161053c
jimmyC <347495739@qq.com>
2023-10-09 22:49:48 +1100

    添加了WebCrosConfig文件

---
+++ Share2Renew_BackEnd/Share2Renew_Server/src/main/java/com/share2renew/config/WebCrosConfig.java
@@ -0,0 +1,23 @@
+package com.share2renew.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @program: Share2Renew_BackEnd
+ * @description:
+ * @author: Junxian Cai
+ **/
+
+@Configuration
+public class WebCrosConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
